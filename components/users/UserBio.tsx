d4d994a69356262f07a2e04e56906b6aebd57819
Eric Ramirez <fabianra5789@gmail.com>
2023-10-30 22:41:42 -0500

    ER: Titles

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -26,19 +26,18 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
       return null;
     }
 
-    return format(new Date(fetchedUser.createdAt), 'MMMM yyyy');
-  }, [fetchedUser?.createdAt])
+    return format(new Date(fetchedUser.createdAt), "MMMM yyyy");
+  }, [fetchedUser?.createdAt]);
 
-
-  return ( 
+  return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button
-            onClick={toggleFollow} 
-            label={isFollowing ? 'Unfollow' : 'Follow'}
+            onClick={toggleFollow}
+            label={isFollowing ? "Unfollow" : "Follow"}
             secondary={!isFollowing}
             outline={isFollowing}
           />
@@ -49,15 +48,11 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <p className="text-white text-2xl font-semibold">
             {fetchedUser?.name}
           </p>
-          <p className="text-md text-neutral-500">
-            @{fetchedUser?.username}
-          </p>
+          <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
         </div>
         <div className="flex flex-col mt-4">
-          <p className="text-white">
-            {fetchedUser?.bio}
-          </p>
-          <div 
+          <p className="text-white">{fetchedUser?.bio}</p>
+          <div
             className="
               flex 
               flex-row 
@@ -65,26 +60,25 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
               gap-2 
               mt-4 
               text-neutral-500
-          ">
+          "
+          >
             <BiCalendar size={24} />
-            <p>
-              Joined {createdAt}
-            </p>
+            <p>Joined {createdAt}</p>
           </div>
         </div>
         <div className="flex flex-row items-center mt-4 gap-6">
           <div className="flex flex-row items-center gap-1">
             <p className="text-white">{fetchedUser?.followingIds?.length}</p>
-            <p className="text-neutral-500">Following</p>
+            <p className="text-neutral-500">Siguiendo</p>
           </div>
           <div className="flex flex-row items-center gap-1">
             <p className="text-white">{fetchedUser?.followersCount || 0}</p>
-            <p className="text-neutral-500">Followers</p>
+            <p className="text-neutral-500">Seguidores</p>
           </div>
         </div>
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default UserBio;
